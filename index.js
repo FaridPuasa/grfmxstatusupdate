f94f08d35922630d73cf845b4df0477394bd52d3
syahmighafar <syahmi.ghafar@globex.com.bn>
2023-10-27 10:28:18 +0800

    fix

--- index.js
+++ index.js
@@ -1692,7 +1692,7 @@ app.post('/updateDelivery', async (req, res) => {
                     FMXAPIrun = 1;
                 }
 
-                if ((req.body.statusCode == 38) && (ccCheck == 1) && (data.data.status == 'custom_clearing') /* && (data.data.instructions == 'CP') */) {
+                if ((req.body.statusCode == 38) && (ccCheck == 1) && (data.data.status == 'custom_clearing') && (data.data.instructions == 'CP')) {
                     var detrackUpdateData = {
                         do_number: consignmentID,
                         data: {
@@ -1706,7 +1706,7 @@ app.post('/updateDelivery', async (req, res) => {
                     FMXAPIrun = 1;
                 }
 
-                if ((req.body.statusCode == 12) && (ccCheck == 1) && (data.data.status == 'custom_clearing') /* && (data.data.instructions == '38') */) {
+                if ((req.body.statusCode == 12) && (ccCheck == 1) && (data.data.status == 'custom_clearing') && (data.data.instructions == '38')) {
                     var detrackUpdateData = {
                         do_number: consignmentID,
                         data: {
