a1982a9f42657a00e2a6f7e3105c4e958d41dea6
syahmighafar <syahmi.ghafar@globex.com.bn>
2024-04-16 07:45:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7671,7 +7671,7 @@ orderWatch.on('change', change => {
 
                     if ((result.length >= 2) && (checkProduct == 0)) {
                         for (let i = 1; i < result.length; i++) {
-                            if (products.includes("localdelivery") == true) {
+                            if ((result[i].product == "localdelivery")||(result[i].product == "localdeliveryjb")) {
                                 if (result[i].product == "localdelivery") {
                                     console.log("sequence is " + result[i].sequence)
                                     if (result[i].sequence == "N/A") {
